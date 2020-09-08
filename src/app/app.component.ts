f0642c47809c9006e16470ebc2ac8e87d2bc6014
waynegakuo <waynejotham@gmail.com>
2020-09-08 14:22:22 +0300

    Importing PDFMake and its fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resumebuilder';
+
+  // tslint:disable-next-line: typedef
+  generatePdf() {
+    const documentDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+    pdfMake.createPdf(documentDefinition).open();
+  }
 }
